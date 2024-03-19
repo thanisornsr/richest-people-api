3d03d086caa8f09ac1147308d438afc9464adf3a
Thansr <thanisornsr@gmail.com>
2024-03-19 18:53:53 +0100

    first commit

---
+++ apiapp.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+richest_people_list = {
+    "Elon Musk": "280 Billion USD",
+    "Jeff Bezos": "250 Billion USD",
+    "Bill Gates": "190 Billion USD",
+    "Mark >ukerberg": "190 Billion USD"
+}
+
+@app.get("/richest-people")
+def richest_people():
+    return richest_people_list
