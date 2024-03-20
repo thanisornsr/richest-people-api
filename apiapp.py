f94bbd57470b1347432ab5b58fd28ab2f676685d
Narongrit Unwerawattana <kicito@users.noreply.github.com>
2024-03-20 09:46:13 +0100

    Correct name

--- apiapp.py
+++ apiapp.py
@@ -6,7 +6,7 @@ richest_people_list = {
     "Elon Musk": "280 Billion USD",
     "Jeff Bezos": "250 Billion USD",
     "Bill Gates": "190 Billion USD",
-    "Mark >ukerberg": "190 Billion USD"
+    "Mark Zuckerberg": "190 Billion USD"
 }
 
 @app.get("/richest-people")
